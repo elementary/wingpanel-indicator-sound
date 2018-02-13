25e3cd52bd86f3555fe689e02515019112d8b98d
Daniel Foré <daniel@elementary.io>
2018-02-13 10:19:57 -0800

    Remove CMake

--- config.vala.cmake
+++
@@ -1,6 +0,0 @@
-namespace Constants {
-public const string INDICATORNAME = "@INDICATORNAME@";
-public const string DATADIR = "@DATADIR@";
-public const string PKGDATADIR = "@PKGDATADIR@";
-public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
-}
